380d3e543d837990b871a4a3a43cbd430b05aa6f
R'Engie <dogboy482@gmail.com>
2021-02-22 09:04:18 -0800

    arrays and stuff

---
+++ script/arrays.js
@@ -0,0 +1,19 @@
+ console.log('hello world');
+
+
+let numbers = [2,4,6,8,10,12,14,16,18,20,22,24,26,28,30,32,34,36,38,40,42,44,46,48,50];
+
+for (let i = 0; i < numbers.length; i++) {
+   console.log('index# is', i,'array value is', numbers[i]) 
+    
+}
+
+
+let people = ['sally', 'sam', 'susan']
+
+for (let e = 0; e < people.length; e++) {
+  if (people[e] == 'sam') {
+    console.log('we found sam, she was in index ',e)  
+  }  
+    
+}
